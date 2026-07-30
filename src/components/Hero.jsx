26c644732eb5871c58517324a04c08dcf63efbd8
AnaghaDolse <anaghadolase08@gmail.com>
2026-07-30 15:28:57 +0530

    Create responsive hero layout using Bootstrap grid

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,26 @@
+import HeroImage from '../assets/hero.png'
+
 const Hero = () => {
   return (
     <>
-      <div className='container py-5'>
-        <h1 className='text-center display-3'>TechConnect 2026</h1>
-        <p className='lead text-center py-2'>
-          Join the biggest developer conference to learn, connect and grow.
-        </p>
-        <div className='d-flex justify-content-center gap-3'>
-          <button className='btn btn-primary me-3'>Register Now</button>
+      <div className='container'>
+        <div className='row align-items-center g-5'>
+          <div className='col-lg-6 text-center text-lg-start'>
+            <h1 className='display-3'>TechConnect 2026</h1>
+            <p className='lead py-2'>
+              Join the biggest developer conference to learn, connect and grow.
+            </p>
+            <div className='d-flex justify-content-center gap-3'>
+              <button className='btn btn-primary'>Register Now</button>
 
-          <button className='btn btn-outline-dark'>Learn More</button>
+              <button className='btn btn-outline-dark'>Learn More</button>
+            </div>
+          </div>
+          <div className='col-lg-6'>
+            <div className='bg-secondary text-white p-5 text-center rounded'>
+              <img src={HeroImage} alt='Event Hero' className='img-fluid' />
+            </div>
+          </div>
         </div>
       </div>
     </>
